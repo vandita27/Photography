b3f139bacb65454b4005ad0049ac2638da87b71f
vandita27 <112273675+vandita27@users.noreply.github.com>
2023-07-01 20:01:48 +0530

    Add files via upload

---
+++ page/contactus.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.toggle').click(function() {
+      $('.sidebar-contact').toggleClass('active');
+      $('.toggle').toggleClass('active');
+      
+    })
+    
+  });
